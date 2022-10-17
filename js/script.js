de633cc48bdad6d98cf7547c00f3f1067bf7341e
happyGGang <joy.gang@dxworks.kr>
2022-10-17 18:28:49 +0900

    [update] 모달오픈 버튼 가져오기

--- js/script.js
+++ js/script.js
@@ -3,4 +3,14 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const $button = get('.modal_open_button')
+
+  const toggleModal = () => {
+    
+  }
+
+  $button.addEventListen('click', () => {
+    toggleModal()
+  })
 })()
