79f54812948fb53fe717a8f8e01eade54b219fc7
happyGGang <joy.gang@dxworks.kr>
2022-10-17 18:46:05 +0900

    [update] 모달 오픈 동작 구현

--- js/script.js
+++ js/script.js
@@ -5,12 +5,15 @@
   }
 
   const $button = get('.modal_open_button')
+  const $modal = get('.modal')
+  const $body = get('body')
 
   const toggleModal = () => {
-    
+    $modal.classList.toggle('show')
+    $body.classList.toggle('scroll_lock')
   }
 
-  $button.addEventListen('click', () => {
+  $button.addEventListener('click', () => {
     toggleModal()
   })
 })()
